f205ed69a4c1b2d9b33a4ed0e5c45a083023ce7a
M-Biban <muskaanbiban@outlook.com>
2025-02-09 16:12:21 +0000

    removing unused imports

--- myweatherapp-tech-test-main/src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ myweatherapp-tech-test-main/src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -4,7 +4,6 @@ import com.weatherapp.myweatherapp.model.CityInfo;
 import com.weatherapp.myweatherapp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
